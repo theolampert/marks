ba308b6b33727e57b44940ef6e6bbe33c0e26453
Theo Lampert <theolampert@gmail.com>
2018-10-24 19:21:32 +0200

    Check in seed directory

---
+++ seed/seed.py
@@ -0,0 +1,18 @@
+import json
+from random import randint
+from faker import Faker
+fake = Faker()
+
+tag_list = ['cs', 'programming', 'color', 'learning', 'design', 'architecture',
+        'bookmarks', 'own-project', 'open-source', 'read-later', 'queue',
+        'a2-german', 'german', 'german-lessions']
+
+bookmarks = []
+
+for _ in range(300):
+    bookmark = {}
+    bookmark['url'] = fake.url()
+    bookmark['tags'] = fake.words(nb=randint(1, 10), ext_word_list=tag_list, unique=True)
+    bookmarks.append(bookmark)
+
+print(json.dumps(bookmarks))
