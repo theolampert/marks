af3da0ca7da99ead169b65701b879782bf610acc
Theo Lampert <theolampert@gmail.com>
2018-10-25 19:20:42 +0200

    Adds state container

--- src/static/main.js
+++ src/static/main.js
@@ -1,56 +1,77 @@
+const dqs = document.querySelector;
 const bookmarkList = document.querySelector('.bookmark-wrapper');
 const form = document.querySelector('.add_bookmark');
 const deleteButtons = document.querySelectorAll('.delete_bookmark');
 const bookmarkLinks = document.querySelectorAll('.bookmark__link');
 
 
+const stateContainer = (initialState, middleware = []) => {
+  let state = initialState;
+
+  const updateState = (newState) => {
+    const nextState = { ...state, ...newState };
+    state = nextState;
+    middleware.map(m => m(nextState));
+    return nextState;
+  }
+
+  return {
+    updateState,
+    getState: () => state,
+  }
+};
+
+const store = stateContainer({
+  bookmarkIndex: -1,
+  nightMode: !!window.localStorage['night-mode'] || false,
+}, [
+  // console.log,
+]);
+
+
 function enableColorMode() {
   const colorModeButton = document.querySelector('.toggle-color-mode');
-  document.body.classList.contains('night-mode')
 
   // Nightmode
-  if (!!window.localStorage.getItem('night-mode')) {
+  if (store.getState().nightMode) {
     document.body.classList.toggle('night-mode');
   }
 
   colorModeButton.addEventListener('click', () => {
     const body = document.body;
-    const isNightMode = body.classList.contains('night-mode');
+    const { nightMode } = store.getState();
 
-    if (!isNightMode) {
+    if (!nightMode) {
       body.classList.add('night-mode');
       localStorage.setItem('night-mode', true);
+      store.updateState({ nightMode: true });
     } else {
       body.classList.remove('night-mode');
       localStorage.removeItem('night-mode');
+      store.updateState({ nightMode: false });
     }
-
   });
 }
 
 function enableShortcuts() {
-  // Vim-ish shortcuts
-  let focusedBookmarkIndex = -1;
-
   bookmarkLinks.forEach((link, index) => {
     link.addEventListener('focus', () => {
-      focusedBookmarkIndex = index;
-    })
+      store.updateState({ bookmarkIndex: index });
+    });
   });
 
   function handleKeys(event) {
     const { key } = event;
     const bookmarks = bookmarkList.childNodes;
+    const index = store.getState().bookmarkIndex;
+    const isBeginningOfList = index < (bookmarkList.childNodes.length - 1);
+    const isEndOfList = index > 0;
 
-    if (key === 'j') {
-      if (focusedBookmarkIndex < (bookmarkList.childNodes.length - 1)) {
-        bookmarks[focusedBookmarkIndex + 1].childNodes[0].focus();
-      }
+    if (key === 'j' && isBeginningOfList) {
+      bookmarks[index + 1].childNodes[0].focus();
     }
-    if (key === 'k') {
-      if (focusedBookmarkIndex > 0) {
-        bookmarks[focusedBookmarkIndex - 1].childNodes[0].focus();
-      }
+    if (key === 'k' && isEndOfList) {
+      bookmarks[index - 1].childNodes[0].focus();
     }
 
   }
